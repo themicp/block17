9c072ebe0205d2479939fb35da053f6a9b9a3f35
Kostis Karantias <karantiaskostis@gmail.com>
2017-03-18 22:28:46 +0200

    Adjust schema

--- api/schema.sql
+++ api/schema.sql
@@ -1,27 +1,33 @@
-create table if not exists `parties` (
-    `name` varchar(191) not null,
-    `iban` varchar(191) not null,
-    `avatar` varchar(191) not null,
-
-    primary key(`iban`)
-);
-
-create table if not exists `transactions` (
-    `id` int not null auto_increment,
-    `timestamp` timestamp default current_timestamp,
-    `counterparty` varchar(191) not null,
-    `description` text,
-    `amount` int not null,
-
-    primary key(`id`),
-    foreign key (`counterparty`)
-        references parties(`iban`)
-);
-
 create table if not exists `banks` (
     `name` varchar(191) not null,
     `btcaddr` varchar(191),
     `endpoint` varchar(191),
 
     primary key(`name`)
+);
+
+create table if not exists `parties` (
+    `name` varchar(191) not null,
+    `iban` varchar(191) not null,
+    `avatar` varchar(191) not null,
+    `bank` varchar(191) not null,
+
+    primary key(`iban`),
+    foreign key (`bank`)
+        references banks(`name`)
+);
+
+create table if not exists `transactions` (
+    `id` int not null auto_increment,
+    `timestamp` timestamp default current_timestamp,
+    `sender` varchar(191) not null,
+    `recipient` varchar(191) not null,
+    `description` text,
+    `amount` int not null,
+
+    primary key(`id`),
+    foreign key (`sender`)
+        references parties(`iban`),
+    foreign key (`recipient`)
+        references parties(`iban`)
 );
