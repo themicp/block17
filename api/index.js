fed5690602ac5ef0b967836cef16f75b763dc71a
Kostis Karantias <karantiaskostis@gmail.com>
2017-03-18 15:46:17 +0200

    Add prototype API methods

--- api/index.js
+++ api/index.js
@@ -5,11 +5,25 @@ const app = module.exports = new koa();
 
 app.use(logger());
 
-const index = (ctx) => {
-    ctx.body = 'hello world';
-};
+app.use(route.get('/tx', (ctx) => {
+    ctx.throw(501);
+}));
 
-app.use(route.get('/', index));
+app.use(route.put('/tx', (ctx) => {
+    ctx.throw(501);
+}));
+
+app.use(route.get('/bank', (ctx) => {
+    ctx.throw(501);
+}));
+
+app.use(route.get('/account', (ctx) => {
+    ctx.throw(501);
+}));
+
+app.use(route.get('/contacts', (ctx) => {
+    ctx.throw(501);
+}));
 
 if (!module.parent) {
     app.listen(3000);
