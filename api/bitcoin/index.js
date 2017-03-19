14222c317eb58df3e859891c80eec4aaffb7558b
Kostis Karantias <karantiaskostis@gmail.com>
2017-03-19 21:00:04 +0200

    🛀🛀🛀🛀🛀🛀🛀🛀🛀

--- api/bitcoin/index.js
+++ api/bitcoin/index.js
@@ -1,9 +1,5 @@
 var exec = require('child_process').exec;
 var request = require('request');
-var EUROBANK_ADDR = '1Q2HwzYwFuWFoYy4TxGuDVrboPaVFrJLjc';
-var BEYONDBANK_ADDR = '12qn3SVU9d5wFMrZXPadw8mRCpYNx9xCmt';
-var EUROBANK_PRIV = 'beyond-hackathon-wallet-1812378912309812-eurobank';
-var BEYONDBANK_PRIV = 'beyond-hackathon-wallet-1812378912309812-beyondbank';
 
 function sendCoins(privKey, toAddress, eurocentAmount) {
     return new Promise((resolve, reject) => {
@@ -35,20 +31,3 @@ function checkReceived(addr) {
 }
 
 module.exports = {sendCoins, checkReceived};
-
-if (!module.parent) {
-    // simple usage example
-    checkReceived(EUROBANK_ADDR).then(
-        (balance) => console.log('Eurobank balance: ' + balance)
-    );
-    checkReceived(BEYONDBANK_ADDR).then(
-        (balance) => console.log('Beyondbank balance: ' + balance)
-    );
-    sendCoins(EUROBANK_PRIV, BEYONDBANK_ADDR, 100).then((stdout) => {
-        console.log('Amount sent successfully:');
-        console.log(stdout);
-    }).catch((err) => {
-        console.log('Failed to send coins:');
-        console.log(err);
-    });
-}
